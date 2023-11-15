469fe6ee20d97ebb3942a8c2660f62a2743f2ff9
miriam <JCE96262@educastur.es>
2023-11-15 21:31:24 +0100

    actualizacion prueba_tomas

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8a/00978dd5f083001e1557ecba87fbc01b
@@ -0,0 +1,10 @@
+package prueba_tomas;
+
+public class Princpal {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+	}
+
+}
---
+++ prueba_tomas/src/prueba_tomas/prueba_tomas.java
@@ -0,0 +1,10 @@
+package prueba_tomas;
+
+public class prueba_tomas {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+	}
+
+}
